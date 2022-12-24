ef1f39ef57743bc9e98436f6bf0a1b584e4085f2
Kiruthik R <104209266+kiruthik07@users.noreply.github.com>
2022-12-24 12:23:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+var randomNum = Math.random();
+
+randomNum = Math.floor( (randomNum * 20) + 1 );
+
+switch (randomNum) {
+    case 1 :
+        document.getElementById("refreshingText").innerHTML="The gift of love. The gift of peace. The gift of happiness. May all these be yours at Christmas.";
+        break;
+    case 2 :
+        document.getElementById("refreshingText").innerHTML="Wishing you a season full of light and laughter for you and your family.";
+        break;
+    case 3 :
+        document.getElementById("refreshingText").innerHTML="Best wishes for a joyous Christmas filled with love, happiness and prosperity!";
+        break;
+    case 4 :
+        document.getElementById("refreshingText").innerHTML="May all that is beautiful, meaningful and brings you joy be yours this holiday season and throughout the coming year!";
+        break;
+    case 5 :
+        document.getElementById("refreshingText").innerHTML="Wishing you all the happiness your holiday can hold!";
+        break;
+    case 6 :
+        document.getElementById("refreshingText").innerHTML="May your holidays sparkle with joy and laughter.";
+        break;
+    case 7 :
+        document.getElementById("refreshingText").innerHTML="I hope the magic of Christmas fills every corner of your heart and home with joy — now and always.";
+        break;
+    case 8 :
+        document.getElementById("refreshingText").innerHTML="Our family wishes you love, joy and peace … today, tomorrow and always.";
+        break;
+    case 9 :
+        document.getElementById("refreshingText").innerHTML="I hope your Christmas is filled with joy this year!";
+        break;
+    case 10 :
+        document.getElementById("refreshingText").innerHTML="Wishing you a Christmas that's merry and bright!";
+        break;
+    case 11 :
+        document.getElementById("refreshingText").innerHTML="May the Lord grant you and all your loved ones peace, joy and goodwill.";
+        break;
+    case 12 :
+        document.getElementById("refreshingText").innerHTML="Wishing you a season that’s merry and bright with the light of God’s love.";
+        break;
+    case 13 :
+        document.getElementById("refreshingText").innerHTML="May God’s blessings be yours this Christmas.";
+        break;
+    case 14 :
+        document.getElementById("refreshingText").innerHTML="Wishing you and your loved ones a blessed Christmas.";
+        break;
+    case 15 :
+        document.getElementById("refreshingText").innerHTML="May the spirit of Christmas be with you all year round.";
+        break;
+    case 16 :
+        document.getElementById("refreshingText").innerHTML="Your love is the best Christmas gift I could ever receive. Merry Christmas, sweetheart!";
+        break;
+    case 17 :
+        document.getElementById("refreshingText").innerHTML="The only thing I love more than Christmas is you.";
+        break;
+    case 18 :
+        document.getElementById("refreshingText").innerHTML="Though we are apart, you are in my heart this Christmas.";
+        break;
+    case 19 :
+        document.getElementById("refreshingText").innerHTML="I'm so lucky to be spending another Christmas with you!";
+        break;
+    case 20 :
+        document.getElementById("refreshingText").innerHTML="Christmas is magical because we're together.";
+        break;
+    default :
+        document.getElementById("refreshingText").innerHTML="May the Christmas Season bring only happiness and joy to you and your family.";
+}
